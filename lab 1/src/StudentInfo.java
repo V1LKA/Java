6d346b684a3a1e122be32f69ab94a2e1a48b42df
V1LKA <adlet.askergeldi27@gmail.com>
2026-01-22 14:18:57 +0500

    Add files via upload

---
+++ lab 1/src/StudentInfo.java
@@ -0,0 +1,22 @@
+public class StudentInfo {
+
+    public static void main(String[] args) {
+
+
+        String fullName = "Әскергелді Әділет Қайратұлы";
+
+
+        String group = "ИС24-21";
+
+
+        String program = "Информационные системы";
+
+        String curs = "2";
+
+        System.out.println("Информация о студенте:");
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Группа: " + group);
+        System.out.println("ОП/Специальность: " + program);
+        System.out.println("Текущий курс:" + curs);
+    }
+}
