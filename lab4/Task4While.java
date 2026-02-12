ba109fbb270eeb21143e954a8fb527726c15f06b
askergeldi adlet <adlet.askergeldi27@gmail.com>
2026-02-12 13:41:59 +0500

    .

---
+++ lab4/Task4While.java
@@ -0,0 +1,14 @@
+package lab4;
+
+public class Task4While {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 5) {
+            System.out.println("Повтор № " + i);
+            i++;
+        }
+    }
+}
