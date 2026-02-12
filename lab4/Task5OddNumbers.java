bb8e51381d2c91d10e57efbb1eb4be88b464743b
askergeldi adlet <adlet.askergeldi27@gmail.com>
2026-02-12 13:41:59 +0500

    .

---
+++ lab4/Task5OddNumbers.java
@@ -0,0 +1,18 @@
+package lab4;
+
+public class Task5OddNumbers {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        System.out.println("Нечётные числа от 1 до 100:");
+
+        do {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+            i++;
+        } while (i <= 100);
+    }
+}
